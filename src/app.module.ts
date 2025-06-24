e2bff75f182a5e73d7a3c92ab66c399bcf76fc57
Lucas Oliveira <lucasjackcosta18@gmail.com>
2025-06-24 19:57:31 -0300

    ✨ feat: config setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { HealthCheckModule } from './modules/health-check/health-check.module';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, HealthCheckModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 import { AppModule } from './app.module';
+
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('v1/brain');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Brain API Documentation')
+    .setDescription('API Brain')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        description: `Insira apenas o token, sem aspas.`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('v1/brain/doc', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      tagsSorter: 'alpha',
+      operationsSorter: 'method',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  console.log(
+    `API Docs: http://localhost:${process.env.PORT ?? 3000}/v1/brain/doc`,
+  );
 }
 bootstrap();
