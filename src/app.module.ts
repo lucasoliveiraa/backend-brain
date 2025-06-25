dd73fc46f8c24e8da8647d7d4293eed890d65733
Lucas Oliveira <lucasjackcosta18@gmail.com>
2025-06-25 19:52:33 -0300

    🐛 bug: adjustment in guard module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/authentication/authentication.guard';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
 import { CultureModule } from './modules/culture/culture.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { FarmModule } from './modules/farm/farm.module';
 import { HarvestModule } from './modules/harvest/harvest.module';
 import { HealthCheckModule } from './modules/health-check/health-check.module';
 import { ProducerModule } from './modules/producer/producer.module';
+import { UserModule } from './modules/user/user.module';
 import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
@@ -14,6 +19,15 @@ import { DatabaseModule } from './shared/database/database.module';
     FarmModule,
     HarvestModule,
     CultureModule,
+    DashboardModule,
+    AuthenticationModule,
+    UserModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
 })
 export class AppModule {}
